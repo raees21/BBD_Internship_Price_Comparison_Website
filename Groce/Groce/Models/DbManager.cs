76c0599b481c002b2748ea47b3e1fc2315b39bf0
Raees Aziz Muhammad <raees@bbd.co.za>
2021-05-10 15:35:50 +0200

    Sql link to Backend

---
+++ Groce/Groce/Models/DbManager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Data
+{
+    public class GroceryContext : DbContext
+    {
+        public GroceryContext(
+            DbContextOptions<GroceryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Groce.Models.Grocery> Groceries{ get; set; }
+    }
+}
--- Groce/Groce/Startup.cs
+++ Groce/Groce/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RazorPagesMovie.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,10 @@ namespace Groce
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddDbContext<GroceryContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("GroceryContextx")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
