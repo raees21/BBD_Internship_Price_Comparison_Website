813b12de84662aa742f762bcc3dbc7347820a735
omoima <omoima@student.wethinkcode.co.za>
2021-05-11 13:10:24 +0200

    New Branch

--- Groce/Groce/Models/Pricing.cs
+++ Groce/Groce/Models/Pricing.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 
 namespace Groce.Models
 {
-    public class Class
+    public class Pricings
     {
+        public double GroceryPrice { get; set; }
+        public int StoreID { get; set; }
+        public int GroceryID { get; set; }
     }
 }
